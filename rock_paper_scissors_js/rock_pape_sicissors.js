de396b55041c523722bd0e57ab16c9b0aa8d8292
Viktor Vuger <vugerviktor22@gmail.com>
2023-08-30 15:18:19 +0200

    hp_bar works

--- rock_paper_scissors_js/rock_pape_sicissors.js
+++ rock_paper_scissors_js/rock_pape_sicissors.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   let playerLives = 5;
   let computerLives = 5;
-  
+
+  const displayText = document.getElementById("player_name");
+
+  const enteredText = prompt("Enter your name:");
+  displayText.textContent = enteredText;
+
   const rock = document.getElementById("rock");
   const paper = document.getElementById("paper");
   const scissors = document.getElementById("scissors");
@@ -9,21 +14,32 @@ document.addEventListener("DOMContentLoaded", function() {
   let playerChoice = null;
   
   rock.addEventListener("click", function() {
-    playerChoice = 1;
-    handleGameLogic(playerChoice);
+    if (playerLives > 0 && computerLives > 0) {
+      playerChoice = 1;
+      handleGameLogic(playerChoice);
+    }
   });
   
   paper.addEventListener("click", function() {
-    playerChoice = 2;
-    handleGameLogic(playerChoice);
+    if (playerLives > 0 && computerLives > 0) {
+      playerChoice = 2;
+      handleGameLogic(playerChoice);
+    }
   });
   
   scissors.addEventListener("click", function() {
-    playerChoice = 3;
-    handleGameLogic(playerChoice);
+    if (playerLives > 0 && computerLives > 0) {
+      playerChoice = 3;
+      handleGameLogic(playerChoice);
+    }
   });
   
   function handleGameLogic(playerChoice) {
+    if (playerLives <= 0 || computerLives <= 0) {
+      // Game has already ended, do nothing
+      return;
+    }
+    
     const getComputerChoice = Math.floor(Math.random() * 3) + 1; // Random number for 3 choices between 1 and 3
     const computerChoice = getComputerChoice;
   
@@ -46,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const playerDamage = (win) => {
       if(win === 'Player wins!'){
         return 1;
-      }else {
+      } else {
         return 0;
       }
     }
@@ -54,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const computerDamage = (win) => {
       if(win === 'Computer wins!'){
         return 1;
-      }else {
+      } else {
         return 0;
       }
     }
@@ -62,20 +78,29 @@ document.addEventListener("DOMContentLoaded", function() {
     playerLives = playerLives - computerDamage(roundWinner);
     console.log(playerLives);
 
+
     computerLives = computerLives - playerDamage(roundWinner);
     console.log(computerLives);
+    
+    document.querySelector('.p1 .in').style.width = (playerLives * (100 / 5)) + '%';
+    document.querySelector('.p2 .in').style.width = (computerLives * (100 / 5)) + '%';
 
     const determineWinner = (lives1 ,lives2) => {
-      if (lives1 === 0){
-        return 'GG u lost!!!!!!!!!!!!!';
-      } else if (lives2 === 0) {
-        return 'GG U WON!!!!!!!!!!!!!!';
+      if (lives1 <= 0){
+        alert('GG u lost!!!!!!!!!!!!!');
+        // Game ended, remove event listeners to prevent further clicks
+        rock.removeEventListener("click");
+        paper.removeEventListener("click");
+        scissors.removeEventListener("click");
+      } else if (lives2 <= 0) {
+        alert('GG U WON!!!!!!!!!!!!!!');
+        // Game ended, remove event listeners to prevent further clicks
+        rock.removeEventListener("click");
+        paper.removeEventListener("click");
+        scissors.removeEventListener("click");
       }
     }
 
-    const winner = determineWinner(playerLives, computerLives);
-    console.log(winner);
+    determineWinner(playerLives, computerLives);
   }
-
-
-  });
+});
